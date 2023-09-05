5e60501f4daa9d56ae03a89724237854cee0193c
thomas <thesaltysweat294@gmail.com>
2023-09-05 11:40:02 -0500

    added voting feature

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
+var artistName = null;
+
 
 window.onload = async function () {
-  console.log("hello world")
   const urlParams = new URLSearchParams(window.location.search);
   const searchValue = urlParams.get('search');
+  artistName = searchValue;
   let result;
 
   const token = localStorage.getItem('token');
@@ -21,7 +23,7 @@ window.onload = async function () {
     .then(data => result = data)
     .catch(error => console.error(error))
   result = JSON.parse(result)
-  
+
   if (401 === result) {
     var errorFont = document.getElementById("errorFont");
     errorFont.innerHTML = `It seems your Token as expired<br> Please make a new session by opening Artists Best in a new tab or press below button to generate a new token`;
@@ -46,10 +48,7 @@ async function generateToken() {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      token: localStorage.getItem('token')
-    })
+    }
   })
     .then(response => response.json())
     .then(data => data)
@@ -58,5 +57,59 @@ async function generateToken() {
   console.log(token)
   localStorage.setItem('token', token);
   window.location.reload();
-  
+
 }
+
+
+function votePop() {
+  document.getElementById("closePopUpButton").style.visibility = "visible";
+  document.getElementById("votePopup").style.visibility = "visible";
+};
+
+
+function closePopup() {
+  document.getElementById("closePopUpButton").style.visibility = "hidden";
+  document.getElementById("votePopup").style.visibility = "hidden";
+};
+
+
+async function confirmVote() {
+  let result = null
+  const searchTerm = document.getElementById("searchBox").value;
+
+  if (searchTerm === ''){
+    return
+  };
+
+
+  const token = localStorage.getItem('token');
+
+
+  await fetch('http://localhost:8000/api/post/vote', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      songName: `${searchTerm}`,
+      artistName: artistName,
+      token: token
+    })
+  })
+    .then(response => response.json())
+    .then(data => result = data)
+    .catch(error => console.error(error))
+
+  result = JSON.parse(result)
+  console.log(result)
+
+  if (401 === result) {
+    var errorFont = document.getElementById("errorFont");
+    errorFont.innerHTML = `It seems your Token as expired<br> Please make a new session by opening Artists Best in a new tab or press below button to generate a new token`;
+    var errorBox = document.getElementById("errorBox");
+    errorBox.style.visibility = "visible";
+    return
+  }
+
+};
+
